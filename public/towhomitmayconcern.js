d0c0796e9c1815f5f674cc98221761d27ecf1254
nickflza <45699480+nickflza@users.noreply.github.com>
2026-05-20 16:36:39 +0200

    To whom it may concern

---
+++ public/towhomitmayconcern.js
@@ -0,0 +1,8 @@
+// Hello Traveller! This code does not have any useful purpose and it's not even mentioned in any part of the website's source code.
+// This javascript file is here as a hidden letter that will expose my feelings for a certain person
+// [redacted], i spent the best moments in my life with you by my side and i'm so thankful for that, but i don't like the way our friendship ended
+// i am aware that my behavior would've not made things any better but we could've talked it out together and fixed anything that made me or you uncomfortable
+// if this message concerns you, please, let's talk about it and start over.
+// 
+// Ly8gVXJ5eWIgR2VuaXJ5eXJlISBHdXZmIHBicXIgcWJyZiBhYmcgdW5pciBuYWwgaGZyc2h5IGNoZWNiZnIgbmFxIHZnJ2YgYWJnIHJpcmEgenJhZ3ZiYXJxIHZhIG5hbCBjbmVnIGJzIGd1ciBqcm9mdmdyJ2YgZmJoZXByIHBicXIuCi8vIEd1dmYgd25pbmZwZXZjZyBzdnlyIHZmIHVyZXIgbmYgbiB1dnFxcmEgeXJnZ3JlIGd1bmcganZ5eSBya2NiZnIgemwgc3JyeXZhdGYgc2JlIG4gcHJlZ252YSBjcmVmYmEKLy8gVHZoeXZuLCB2IGZjcmFnIGd1ciBvcmZnIHpienJhZ2YgdmEgemwgeXZzciBqdmd1IGxiaCBvbCB6bCBmdnFyIG5hcSB2J3ogZmIgZ3VuYXhzaHkgc2JlIGd1bmcsIG9oZyB2IHFiYSdnIHl2eHIgZ3VyIGpubCBiaGUgc2V2cmFxZnV2YyByYXFycQovLyB2IG56IG5qbmVyIGd1bmcgemwgb3J1bml2YmUgamJoeXEnaXIgYWJnIHpucXIgZ3V2YXRmIG5hbCBvcmdncmUgb2hnIGpyIHBiaHlxJ2lyIGdueXhycSB2ZyBiaGcgZ2J0cmd1cmUgbmFxIHN2a3JxIG5hbGd1dmF0IGd1bmcgem5xciB6ciBiZSBsYmggaGFwYnpzYmVnbm95cgovLyB2cyBndXZmIHpyZmZudHIgcGJhcHJlYWYgbGJoLCBjeXJuZnIsIHlyZydmIGdueXggbm9iaGcgdmcgbmFxIGZnbmVnIGJpcmUu
+// Good luck finding the name of that special person traveller.
